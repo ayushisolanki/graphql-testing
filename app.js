ffbc379ada4ef52d9e252c7ef9f51e33a9b83116
Ayushi Solanki <ayushisolanki@Ayushis-MacBook-Air.local>
2021-10-07 12:23:19 +0530

    first

---
+++ app.js
@@ -0,0 +1,46 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+ 
+  type Book {
+    
+    title: String
+    author: String 
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+const books = [
+    {
+      // id:1,
+      title: 'The Awakening',
+      author: 'Kate Chopin',
+    },
+    {
+      // id:2,
+      title: 'City of Glass',
+      author: 'Paul Auster',
+    },
+  ];
+
+ 
+const resolvers = {
+    Query: {
+      books: () => books,
+    },
+  };
+  
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
+
+function sum(a,b){
+  return a+b;
+}
+
+module.exports = sum;
