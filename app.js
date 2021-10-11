604b366de4c702f27eba138251c1144cf720b0aa
Ayushi Solanki <ayushisolanki@Ayushis-MacBook-Air.local>
2021-10-11 10:48:25 +0530

    third

--- app.js
+++ app.js
@@ -3,23 +3,31 @@ import { ApolloServer, gql } from 'apollo-server';
 const typeDefs = gql`
  
   type Book {
-    
-    title: String
-    author: String 
+    id: ID!
+    title: String!
+    author: String!
   }
 
   type Query {
     books: [Book]
+    book(id:ID):Book
   }
+
+  type Mutation {
+    createBook(id: ID!, title:String!, author:String!) : Book!
+    updateBook(id: ID!, title:String!, author:String!) : Book!
+    deleteBook(id: ID!): Book!
+  }
+
 `;
 const books = [
     {
-      // id:1,
+      id:1,
       title: 'The Awakening',
       author: 'Kate Chopin',
     },
     {
-      // id:2,
+      id:2,
       title: 'City of Glass',
       author: 'Paul Auster',
     },
@@ -28,8 +36,39 @@ const books = [
  
 const resolvers = {
     Query: {
-      books: () => books,
+      book: (parent, { id }, context, info) => {
+        return books.find(book => book.id == id);
+      },
+      books: (parent, args, context, info) => {
+        return books;
+      }
     },
+    Mutation:{
+      createBook: (parent, { id, title, author}, context, info) => {
+        const newBook = { id, title, author };
+  
+        books.push(newBook);
+  
+        return newBook;
+      },
+      updateBook:(parent, { id, title, author}, context, info) => {
+        let newBook = books.find(book => book.id == id);
+
+        newBook.author = author;
+        newBook.title = title;
+        
+        return newBook;
+      },
+      deleteBook:(parent, { id}, context, info) => {
+
+        const bookIndex = users.findIndex(book => book.id == id);
+
+        if (bookIndex == -1) throw new Error("Book not found.");
+
+        books.splice(bookIndex, 1);
+        return books;
+      }
+    }
   };
   
 
@@ -39,6 +78,6 @@ server.listen().then(({ url }) => {
   console.log(`Server ready at ${url}`);
 });
 
-export function sum(a,b){
-  return a+b;
-}
+// export function sum(a,b){
+//   return a+b;
+// }
--- tests/api.spec.js
+++ tests/api.spec.js
@@ -1,4 +1,5 @@
 import { GraphQLClient } from "graphql-request";
+
 /* import { sum } from "../app.js";
  */
 const graphqlClient = new GraphQLClient("http://localhost:4000");
@@ -14,12 +15,54 @@ describe("Books API",() => {
     test("Returns all books", async ()=> {
         const res = await graphqlClient.request(`{
             books {
-              author
+              id
             }
           }`, {});
         const { books } = res;
         expect(books).toHaveLength(2);
-    })
-})
+    });
+
+    test("Returns book with ID=1", async ()=> {
+      const res = await graphqlClient.request(`{
+          book(id:1) {
+            id
+            title
+            author
+          }
+        }`, {});
+
+      // const { books } = res;
+      console.log("Returned book with ID=1 => ", res);
+      // expect(res).toHaveProperty('id');
+    });
+
+    test("Adds a book", async ()=> {
+      const res = await graphqlClient.request(`
+      mutation{
+        createBook(id: 3, title: "Game of Thrones", author:"George Martin") {
+          id
+          title
+          author
+        }
+        }`, {});
+      // const { books } = res;
+      console.log("Added a book => ", res);
+      // expect(books).toHaveLength(1);
+    });
+
+    test("Updates a book", async ()=> {
+      const res = await graphqlClient.request(`
+      mutation{
+        createBook(id: 1, title: "Game of Thrones", author:"Ayushi") {
+          id
+          title
+          author
+        }
+        }`, {});
+      // const books = res;
+      console.log("Updated a book => ", res);
+      // expect(books).toHaveLength(1);
+    });
+});
 
 
